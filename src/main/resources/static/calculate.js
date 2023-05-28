7efd0d29d53ef7a890be20ecf5b6caf43c641d52
Derek <chibie020@gmail.com>
2023-05-27 20:15:08 -0700

    Second commit with error message validation on side of input & mobile-first approach on mainarea flex box

--- src/main/resources/static/calculate.js
+++ src/main/resources/static/calculate.js
@@ -18,129 +18,136 @@ const gradeLetters = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D", "F
 
 // get max bound label element
 let maxBoundLabelElement = document.getElementById("maxBoundLabel");
-let maxBoundInputElement = document.getElementById(maxBoundLabelElement.htmlFor);
-/*  --------------  --------------  --------------  -------------- */
-
-// handle toggle for new grade prompt
-promptIsEnabled = document.getElementById("enablePromptToggle").addEventListener("change", (ev) => {
-    promptIsEnabled = !promptIsEnabled;
-    // ev.target.checked
-    console.log("promptIsEnabled:", promptIsEnabled);
-});
-
-// number validation function
-let isValidNumber = (value) => {
-    let isValid = true;
-    if (value < 0 || value > parseInt(maxBoundInputElement.value)){
-        alert("No change made. Please enter a valid number between 0 and Max(" + maxBoundInputElement.value + ")");
-        isValid = false;
-    }
-    return isValid;
-};
+let maxBoundInputElement = document.getElementById(`${maxBoundLabelElement.htmlFor}`);
 
 // set/initialize the default data values and show the histogram
 gradeLetters.map((gletter, i) => gradeBounds.set(gletter, defaultGradeBounds[i]));
 showHistogram(gradeBounds, grades);
+/*  --------------  --------------  --------------  -------------- */
 
-// Handle bounds inputs
-// get all elements of the "boundLabel" name and their associated input elements
-let boundLabelElements = document.getElementsByName("boundLabel");
-// iterate over boundLabelElements to access their corresponding input elements
-boundLabelElements.forEach((ele, i) => {
-    let associatedInputElementId = ele.htmlFor;
-    // handle if its associatedInputElement's value changes
-    document.getElementById(`${associatedInputElementId}`).addEventListener("change", (ev) => {
-        try {
-            let associatedInputElementValue = ev.target.value;
-            let thisLabelGradeLetter = ele.innerHTML;
+// handle toggle for new grade prompt
+// promptIsEnabled = document.getElementById("enablePromptToggle").addEventListener("change", (ev) => {
+//     promptIsEnabled = !promptIsEnabled;
+//     // ev.target.checked
+//     console.log("promptIsEnabled:", promptIsEnabled);
+// });
 
-            if (isValidNumber(associatedInputElementValue)){
-                // update the gradeBounds map's grade letter bound
-                gradeBounds.set(thisLabelGradeLetter, associatedInputElementValue);
 
+// Handle "new grade" input box
+let newGradeInputBox = document.getElementById('newGradeInputBox');
+newGradeInputBox.addEventListener("change", evChange => {
+    console.log("change!");
+    handleShowAlertMsg(evChange.target, isValidNumber(evChange.target));
+});
+newGradeInputBox.addEventListener('keydown', (evKeyDown) => handleNewGradeInput(evKeyDown, gradeBounds, grades));
+function handleNewGradeInput(evKeyDown, gradeBounds, grades){
+    try {
+        let newValue = parseInt(evKeyDown.target.value);
+        let isValidNumberEntered = isValidNumber(evKeyDown.target);
+        if (evKeyDown.key == "Enter") {
+            console.log("Enter keyPress!");
+            // this input element's value as well as the values of the bounds should be valid
+            if (isValidNumberEntered){
+                // push value to grades array
+                grades.push(newValue);
                 // show updated histogram
                 showHistogram(gradeBounds, grades);
-            }            
-        }
-        catch(error){
-            console.error("Error on key press in ", error)
-        }
-    });
-});
-
-// validate all lower bounds input values
-function isAllBoundsValid(bounds){
-    let boundLabelElements = document.getElementsByName("boundLabel");
-    let allBoundsAreValid = true; 
-    // iterate over boundLabelElements to access their corresponding input elements
-    boundLabelElements.forEach((ele, i) => {
-        let associatedInputElementId = ele.htmlFor;
-        // handle if its associatedInputElement's values are valid
-        let associatedGradeBoundInputValue = document.getElementById(`${associatedInputElementId}`).value;
-        if (!isValidNumber(associatedGradeBoundInputValue)){
-            alert("One or more of the bounds has an invalid value. Change it");
-            allBoundsAreValid = false;            
-        }
-    });
-    return allBoundsAreValid;
-}
-
-// Handle the New Grade input box for new grades entered
-let newGradeInputBox = document.getElementById('newGradeInputBox');
-newGradeInputBox.addEventListener('keydown', (ev) => {
-    try {
-        let newValue = ev.target.value;
-        if (ev.key === "Enter") {
-            // new grade input validation
-            if (isValidNumber(newValue)){
-                // also check if the bounds text boxes have valid numbers
-                if (isAllBoundsValid()){
-                    // is prompt toggle option enabled?
-                    if (document.getElementById("enablePromptToggle").checked){
-                        if (confirm(`Add ${newValue} as a new grade score`)){
-                            console.log("Okay!");
-                            // add the new grade
-                            grades.push(parseInt(newValue));
-
-                            // show updated histogram
-                            showHistogram(gradeBounds, grades);
-                        }
-                        else{
-                            // do not add the new grade
-                            console.log("Cancel!");
-                        }
-                    }
-                    else {
-                        // add the new grade
-                        grades.push(parseInt(newValue));
-
-                        // show updated histogram
-                        showHistogram(gradeBounds, grades);
-                    }
-                }                
+                console.log("Enter keyPress!", grades, maxBoundInputElement.value, newValue);
+            }else {
+                handleShowAlertMsg(evKeyDown.target, isValidNumberEntered);
             }
-            console.log("Enter keyPress!", grades, maxBoundInputElement.value, newValue);
-        }
-        else {
-            console.log("keyPress!");
         }
     }
     catch(error){
         console.error("Error on key press in ", error)
     }
+};
+
+// Handle "lower bounds" input boxes
+let boundLabels = document.getElementsByName('boundLabel');
+boundLabels.forEach((labelElement) => {
+    let letterGrade = labelElement.innerHTML;
+    let associatedInputElement = document.getElementById(`${labelElement.htmlFor}`);
+    associatedInputElement.addEventListener('change', (evChange) => handleBoundInput(evChange, letterGrade, gradeBounds, grades));
 });
+function handleBoundInput(evChange, gradeLetter, gradeBounds, grades) {
+    let boundValue = parseInt(evChange.target.value);
+    let isValidNumberEntered = isValidNumber(evChange.target);
+    if (isValidNumberEntered){
+        gradeBounds.set(gradeLetter, boundValue);
+        console.log("Bound input Change", gradeBounds);
+    }
+    showHistogram(gradeBounds, grades);
+    handleShowAlertMsg(evChange.target, isValidNumberEntered);
+}
 
+// number validation function
+let isValidNumber = (targetElement) => {
+    let valueIsNegative = parseInt(targetElement.value) < 0;
+    let valueIsGreaterThanMax = parseInt(targetElement.value) > parseInt(maxBoundInputElement.value);
+    let result = (!valueIsNegative && !valueIsGreaterThanMax);
+    // handle whether to show or hide the alert error message for a particular input box
+    return result;
+};
 
-// handle max bound value change
-maxBoundInputElement.addEventListener("change", (ev) => {
+function handleShowAlertMsg(targetElement, isValid) {
+    
+    console.log("isValid:", isValid);
+    // iterate through all "output" elements to find the one associated with this target element
+    document.getElementsByName("alertMsg")
+    .forEach(alertMsgEle => {
+        let doRemoveErrorMsg = !isValid;
+        if (alertMsgEle.htmlFor.value === targetElement.id){
+            // hide or show the alert message
+            if (doRemoveErrorMsg){
+                alertMsgEle.classList.remove("hiddenAlertMsg");
+            }
+            else {
+                alertMsgEle.classList.add("hiddenAlertMsg");
+            }
+        }
+    });
+    if (isValid){
+        targetElement.classList.remove("inputErrorFound");
+    }
+    else {
+        targetElement.classList.add("inputErrorFound");
+    }    
+}
+
+// validate all lower bounds input values
+function isAllBoundsValid(){
+    let boundLabelElements = document.getElementsByName("boundLabel");
+    let allBoundsAreValid = true;
+    // iterate over boundLabelElements to access their corresponding input elements
+    boundLabelElements.forEach((ele) => {
+        let associatedInputElementId = ele.htmlFor;
+        // handle if its associatedInputElement's values are valid
+        let associatedGradeBoundInputBox = document.getElementById(`${associatedInputElementId}`);
+        let isValidNumberEntered = isValidNumber(associatedGradeBoundInputBox);
+        if (!isValidNumberEntered){
+            // each element will be validated and a singl invalid one will set this variable to false
+            allBoundsAreValid = false;
+        }
+    });
+    return allBoundsAreValid;
+}
+
+// handle adjusting max of each bound
+document.getElementById("maxBoundInput")
+.addEventListener("keydown", (ev) => {
+    console.log("change max");
     try {
-        boundLabelElements.forEach((ele, i) => {
-            let associatedInputElementId = ele.htmlFor;
+        console.log("isAllBoundsValid():", isAllBoundsValid());
+        // handles bounds when max changed
+        document.getElementsByName('boundInput').forEach((targetElement) => {
             // set the max for each bound input element
             let newMax = ev.target.value;
-            let associatedInputElement = document.getElementById(`${associatedInputElementId}`);
-            associatedInputElement.max = newMax;
+            targetElement.max = newMax;
         });
+        // handles "new grade" input error when max changed
+        let newGradeInputBox = document.getElementById("newGradeInputBox");
+        handleShowAlertMsg(newGradeInputBox, isValidNumber(parseInt(newGradeInputBox.value)));
         // update histogram display
         showHistogram(gradeBounds, grades);
     } catch (error) {
@@ -170,7 +177,7 @@ function updateGradesToFreqMap(gradesBoundsMap, gradesArray, gradesTofrequencyMa
             // count the freq within each bound/range
             const freqInThisBound = gradesArray.reduce(
                 // higher/upperBound exclusive & lower bound inclusive
-                (accum, currValue) => ((higherBound == maxBoundInputElement.value && currValue <= higherBound) || (higherBound != maxBoundInputElement.value && currValue < higherBound)) && currValue >= value ? accum + 1 : accum , initCount);
+                (accum, currValue) => ((higherBound === maxBoundInputElement.value && currValue <= higherBound) || (higherBound != maxBoundInputElement.value && currValue < higherBound)) && currValue >= value ? accum + 1 : accum , initCount);
 
             // update grades to freq map
             gradesTofrequencyMap.set(keyLetterGrade, freqInThisBound);
